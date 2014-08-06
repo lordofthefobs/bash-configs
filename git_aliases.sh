ac9df50e971e32f980d76e0e782a748052aa3ded
Sang Park <sang.park@locationlabs.com>
2014-08-06 12:19:17 -0700

    adding git command to work with clipboard

--- git_aliases.sh
+++ git_aliases.sh
@@ -2,4 +2,6 @@ alias g.c="git checkout"
 alias g.c.d="git checkout develop"
 alias g.c.b="git checkout bug/$1"
 alias g.c.ba="git checkout bug/ATTCNI-$1"
+alias g.clipboard.checkout='g.clipboard checkout'
+alias g.clipboard="git $1 $(pbpaste)"
 
