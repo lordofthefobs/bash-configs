2dd75c416788f6c519cf778ef7be1bfdac93b832
Sang Park <sang.park@locationlabs.com>
2018-03-13 11:00:01 -0700

    fixing merge conflict

--- initialize.sh
+++ initialize.sh
@@ -1,13 +1,4 @@
-export PATH=/Users/sang.park/bin:/bin:/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin:$PATH
-export EDITOR='subl -w'
-
-export BASH_CONFIG_DIR="$HOME/dev/bash-configs"
-
-export DISABLE_LL_PROMPT='false'
-
-alias reload-all="load_profiles && load_aliases"
-alias reload-profiles="load_profiles"
-alias reload-aliases="load_aliases"
+#!/bin/bash
 
 function load_file() {
    if [ -f "$BASH_CONFIG_DIR/$1" ]; then
@@ -21,19 +12,13 @@ function load_profiles() {
 }
 
 function load_aliases() {
-	# load bash alias file
-	load_file load_aliases.sh
+  # load bash alias file
+  load_file load_aliases.sh
 }
 
 load_profiles
 load_aliases
 
-
-#THIS MUST BE AT THE END OF THE FILE FOR GVM TO WORK!!!
-[[ -s "/Users/sang.park/.gvm/bin/gvm-init.sh" ]] && source "/Users/sang.park/.gvm/bin/gvm-init.sh"
-
-[ -s "/Users/sang.park/.nvm/nvm.sh" ] && . "/Users/sang.park/.nvm/nvm.sh" # This loads nvm
-
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
-
-test -e "${HOME}/.iterm2_shell_integration.bash" && source "${HOME}/.iterm2_shell_integration.bash"
+alias reload-all="load_profiles && load_aliases"
+alias reload-profiles="load_profiles"
+alias reload-aliases="load_aliases"
