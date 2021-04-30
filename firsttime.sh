95e0b38e68961da5b5703f3c8ad22c6f13b44f95
Sang Park <sang.park@veeva.com>
2021-04-30 10:43:06 -0700

    added firstime scripts

--- firsttime.sh
+++ firsttime.sh
@@ -1,8 +1,17 @@
 # setup shell file
 
+
+function load_file() {
+  if [[ -f "$BASH_CONFIGS_DIR/$1" ]]; then
+   	source "$BASH_CONFIGS_DIR/$1"
+  fi
+}
+
 if [ -n $ZSH_NAME ]; then
 	echo "setting up ZSH"
 	echo 'export BASH_CONFIGS_DIR="/Users/sangpark/dev/personal/bash-configs"' >> ~/.zshrc
 
 	echo '. $BASH_CONFIGS_DIR/initialize.sh' >> ~/.zshrc
 fi
+
+laod_file git/firsttime.sh
